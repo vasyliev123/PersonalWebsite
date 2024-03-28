3c04f1140290e93dc4609c9d2efc16ce0674bf59
Dmytro Vasyliev <vasylievdmytro04@gmail.com>
2024-03-27 21:21:22 -0700

    configured skeleton and purgecss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<LightSwitch />
